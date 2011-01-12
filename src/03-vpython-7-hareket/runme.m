6f8490985eff8f520129330223da36ba9939d5a3
nurettin senyer <nurettin.senyer@bil.omu.edu.tr>
2011-01-12 21:15:40 +0200

    debug kodu konuldu

--- src/03-vpython-7-hareket/runme.m
+++ src/03-vpython-7-hareket/runme.m
@@ -1,17 +1,44 @@
-clc;
+clear all;  close all;  clc;
 
-anlik = [1:5:75 70:-10:-70 -60:20:90];
+dbg = true;
 
-nums = anlik;
+fnm_alpha = pathos('_bkp/renkli_marker_alpha.csv');
+if ~exist(fnm_alpha),
+    error(sprintf('19bal/heg/.../05-vid-renkli-marker/_bkp/%s yi buraya kopyala', fnm_alpha));  
+end
 
-for i = 1:length(nums)
+alpha = csvread(fnm_alpha);
+
+% preprocess
+% a) NaN
+idx = find(isnan(alpha));
+alpha(idx) = alpha(idx-1);
+
+% b) 0
+idx = find(alpha==0);
+alpha(idx) = alpha(idx-1);
+
+alpha = alpha - mean(alpha);
+alpha = round(alpha);
+
+if dbg, plot(alpha),    end
+
+% ilkleme
+fid = fopen('test.txt', 'a');
+tmp = alpha(1);
+fprintf(fid, '%d\n', tmp);
+fclose(fid);
+pause(1)
+
+for i = 1:length(alpha)
     fid = fopen('test.txt', 'a');
-    %tmp = 6+randperm(2);
-    %tmp = tmp(1);
-    tmp = nums(i);
+
+    tmp = alpha(i);
     
     fprintf(fid, '%d\n', tmp);
     fclose(fid);
+    
     fprintf('%s : %d\n', datestr(now, 'MM:SS:FFF'), tmp);
     pause(0.1)
 end
+
